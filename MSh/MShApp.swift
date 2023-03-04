9a4acf09f156506fbdd558b4fee91bb12d690e6e
Maximilian Schwenger <schwengermaximilian@gmail.com>
2023-03-04 16:13:53 +0100

    Initial Commit

---
+++ MSh/MShApp.swift
@@ -0,0 +1,17 @@
+//
+//  MShApp.swift
+//  MSh
+//
+//  Created by Maximilian Schwenger on 04.03.23.
+//
+
+import SwiftUI
+
+@main
+struct MShApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
